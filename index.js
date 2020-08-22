53d7fb7e69fa4fffeeb08ca525c28ac5b4ab126c
Jason Dictos <jdictos@aparavi.com>
2020-08-22 14:20:11 -0700

    WIP

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Require google from googleapis package.
 const { google } = require('googleapis')
+const fs = require('fs')
 
 // Require oAuth2 from our google instance.
 const { OAuth2 } = google.auth
@@ -18,62 +19,23 @@ oAuth2Client.setCredentials({
 // Create a new calender instance.
 const calendar = google.calendar({ version: 'v3', auth: oAuth2Client })
 
-// Create a new event start date instance for temp uses in our calendar.
-const eventStartTime = new Date()
-eventStartTime.setDate(eventStartTime.getDay() + 2)
+// Load the schedule
+const schedule = JSON.parse(fs.readFileSync('./schedule.json'));
 
-// Create a new event end date instance for temp uses in our calendar.
-const eventEndTime = new Date()
-eventEndTime.setDate(eventEndTime.getDay() + 4)
-eventEndTime.setMinutes(eventEndTime.getMinutes() + 45)
+// Get the start of the alternate weekend schedule
+const { start, range } = schedule.alternateWeekends;
 
-// Create a dummy event for temp uses in our calendar
-const event = {
-  summary: `Meeting with David`,
-  location: `3595 California St, San Francisco, CA 94118`,
-  description: `Meet with David to talk about the new client project and how to integrate the calendar for booking.`,
-  colorId: 1,
-  start: {
-    dateTime: eventStartTime,
-    timeZone: 'America/Denver',
-  },
-  end: {
-    dateTime: eventEndTime,
-    timeZone: 'America/Denver',
-  },
-}
+console.log('Alternate weekends starting with dad on:')
+console.log(`  Start: ${start}`);
+console.log(`  Range: ${range}`);
 
-// Check if we a busy and have an event on our calendar for the same time.
-calendar.freebusy.query(
-  {
-    resource: {
-      timeMin: eventStartTime,
-      timeMax: eventEndTime,
-      timeZone: 'America/Denver',
-      items: [{ id: 'primary' }],
-    },
-  },
-  (err, res) => {
-    // Check for errors in our query and log them if they exist.
-    if (err) return console.error('Free Busy Query Error: ', err)
-
-    // Create an array of all events on our calendar during that time.
-    const eventArr = res.data.calendars.primary.busy
-
-    // Check if event array is empty which means we are not busy
-    if (eventArr.length === 0)
-      // If we are not busy create a new calendar event.
-      return calendar.events.insert(
-        { calendarId: 'primary', resource: event },
-        err => {
-          // Check for errors and log them if they exist.
-          if (err) return console.error('Error Creating Calender Event:', err)
-          // Else log that the event was created.
-          return console.log('Calendar event successfully created.')
-        }
-      )
-
-    // If event array is not empty log that we are busy.
-    return console.log(`Sorry I'm busy...`)
+// Now walk the weekly events
+for (const { months, events} of schedule.weekly) {
+  console.log('');
+  console.log(`For the months of ${months}:`);
+  for (const event of events) {
+    console.log(`  Parent: ${event.parent} (${schedule.parents[event.parent].name})`);
+    console.log(`    Start: ${event.start}`);
+    console.log(`    End: ${event.end}`);
   }
-)
+}
