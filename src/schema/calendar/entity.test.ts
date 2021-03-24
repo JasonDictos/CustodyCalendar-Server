5f87bc87689ab8adb02f0af1829b4f7abb3f58d5
Jason Dictos <jason@dictos.com>
2021-03-24 10:51:48 -0700

    Improve tests

--- src/schema/calendar/entity.test.ts
+++ src/schema/calendar/entity.test.ts
@@ -19,25 +19,46 @@ describe("schema.calendar.entity", function()  {
 	test("Location", async function()   {
 		await expect(model.count()).resolves.toBe(0)
 
-		const id = await model.insert({ type: entity.Type.Location, fields: {
+		const dadId = await model.insert({ type: entity.Type.Location, fields: {
 			name: "Dad's House",
 			address: "1234"
 		} as entity.Location})
 
+		console.info(`Dad's house ${dadId}`)
+
 		await expect(model.count()).resolves.toBe(1)
-		const entities = await model.select()
+		let entities = await model.select()
 		expect(entities.length).toBe(1)
-		const location = entities[0].fields as entity.Location
+		let location = entities[0].fields as entity.Location
 		expect(location.address).toBe("1234")
 		expect(location.name).toBe("Dad's House")
 
-		const row = await model.get(id)
-		expect(row.id).toBe(id)
+		let row = await model.get(dadId)
+		expect(row.id).toBe(dadId)
 		expect(row.type).toBe(entity.Type.Location)
 		expect(row.fields).toEqual(location)
 
-		await model.delete(id)
+		const momId = await model.insert({ type: entity.Type.Location, fields: {
+			name: "Moms's House",
+			address: "2345"
+		} as entity.Location})
 
+		await expect(model.count()).resolves.toBe(2)
+		entities = await model.select()
+		expect(entities.length).toBe(2)
+
+		console.info(`Moms's house ${momId}`)
+
+		row = await model.get(momId)
+		expect(row.id).toBe(momId)
+		expect(row.type).toBe(entity.Type.Location)
+		location = row.fields as entity.Location
+		expect(location.address).toBe("2345")
+		expect(location.name).toBe("Moms's House")
+
+		await model.delete(momId)
+		await expect(model.count()).resolves.toBe(1)
+		await model.delete(dadId)
 		await expect(model.count()).resolves.toBe(0)
 	})
 })
